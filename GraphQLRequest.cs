71107abfdc165aeb34b507065f3b8a047ba75404
Marcelo Pereira da Silva <marcelopspereira@gmail.com>
2018-04-21 00:25:34 -0300

    config melhoria Request

---
+++ GraphQLRequest.cs
@@ -0,0 +1,7 @@
+namespace graphql_teste
+{
+    public class GraphQLRequest
+    {
+        public string Query { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraphQL;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace graphql_teste
 {
@@ -23,34 +25,31 @@ namespace graphql_teste
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            var schema = new Schema { Query = new HelloWorldQuery() };
+
             app.Run(async (context) =>
-            {
-                var result = await new DocumentExecuter().ExecuteAsync(doc =>
                 {
-                    doc.Schema = schema;
-                    doc.Query = @"
-                    query {
-                        hello
-                        howdy
+                    if (context.Request.Path.StartsWithSegments("/api/graphql") && string.Equals(context.Request.Method, "POST", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string body;
+                        using (var streamReader = new StreamReader(context.Request.Body))
+                        {
+                            body = await streamReader.ReadToEndAsync();
+
+                            var request = JsonConvert.DeserializeObject<GraphQLRequest>(body);
+                            var schema = new Schema { Query = new HelloWorldQuery() };
+
+                            var result = await new DocumentExecuter().ExecuteAsync(doc =>
+                            {
+                                doc.Schema = schema;
+                                doc.Query = request.Query;
+                            }).ConfigureAwait(false);
+
+                            var json = new DocumentWriter(indent: true).Write(result);
+                            await context.Response.WriteAsync(json);
+                        }
                     }
-                ";
-                }).ConfigureAwait(false);
-
-                var json = new DocumentWriter(indent: true).Write(result);
-                await context.Response.WriteAsync(json);
-            });
-        //     if (env.IsDevelopment())
-        //     {
-        //         app.UseDeveloperExceptionPage();
-        //     }
-
-            //     app.Run(async (context) =>
-            //     {
-            //         await context.Response.WriteAsync("Hello World!");
-            //     });
+                });
         }
-
 
     }
 }
